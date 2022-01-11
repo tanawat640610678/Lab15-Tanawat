d0933dfed0f324c28a91aec9f90ad15b97e73dbf
tanawat640610678 <thanawat_chai@cmu.ac.th>
2022-01-11 16:25:54 +0700

    Lab15_1.cpp Complete

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,9 +3,29 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+		
+		for(int i =1 ; i < N ; i++)
+        {
 
+            T key = d[i];
+             int x = i-1;
+
+	         while(x >= 0 && d[x] < key)
+	         { 
+		         d[x+1] = d[x];
+		         x--;
+		         d[x+1] = key;
+	         }     
+        
+	         cout  << "Pass " << i << ":";
+		  
+		     for(int i =0 ; i < N ; i++)
+		     {
+			      cout << d[i] << " ";
+		     }
+		        cout <<"\n";
+        }
 }
-
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
 	cout << "Input Array:";
@@ -17,3 +37,8 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
+
+
+
+
