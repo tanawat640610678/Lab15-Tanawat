6ad35bc469d03888a4cde5976050ebf484bc9fbc
tanawat640610678 <thanawat_chai@cmu.ac.th>
2022-01-11 17:46:27 +0700

    Lab15_2.cpp Complete

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,44 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool d[][N]){
+
+	for(int x = 0; x < N ; x++){
+		for(int y = 0 ; y < N ; y++){
+
+	          cout << d[x][y] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+void inputMatrix(double k[][N]){
+
+	  for(int i = 0 ; i < N ; i++){
+		  cout << "Row "<<i+1<<": ";
+		  for(int j = 0; j < N ; j++){
+			  cin >> k[i][j];
+		  }
+	  }
+}
+
+void findLocalMax(const double I[][N], bool O[][N]){
+
+	for(int a = 0 ; a < N ; a++ )
+	{
+		for(int b = 0 ; b < N ; b++)
+		{
+                O[a][b] = false;
+		}
+	}
+
+    for(int a = 1 ; a < N-1 ; a++)
+	{
+        for(int b = 1 ; b < N-1 ; b++)
+		{
+            if(I[a][b] >= I[a-1][b] && I[a][b] >= I[a+1][b] && I[a][b] >= I[a][b-1] && I[a][b] >= I[a][b+1])
+			O[a][b] = true;
+		}
+	}
+}
